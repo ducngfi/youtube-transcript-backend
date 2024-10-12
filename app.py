7277ae8839bc55f1554784cd80e8298ff4de737f
ducngfi <133224794+ducngfi@users.noreply.github.com>
2024-10-12 22:09:18 +0300

    12389

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def test():
     return jsonify({"message": "Flask server is running"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
